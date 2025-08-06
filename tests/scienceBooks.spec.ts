d91daec34caab3afb151aac41892d42650b0ffbc
Irfan Sheriff <itsirfandude@gmail.com>
2025-08-06 20:55:07 +0530

    Update scienceBooks.spec.ts with total books and in stock count

--- tests/scienceBooks.spec.ts
+++ tests/scienceBooks.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import fs from "fs";
+//import fs from "fs";
 interface Book {
   title: string | null;
   price: string | null;
@@ -13,36 +13,48 @@ test('BookScrape', async ({ page }) => {
   await expect(page.getByText(" We love being scraped!")).toBeVisible();
 
   // Click the get started link.
-  await page.getByRole('link', { name: ' Science' , exact: true }).click();
+  await page.getByRole('link', { name: ' Science', exact: true }).click();
 
   await expect(page.getByRole("heading", { name: "Science" })).toBeVisible();
-  
 
-// 1. Grab all book elements
-const books = page.locator('.product_pod');
 
-// 2. Prepare an array to store data
-let bookDetails: Book[] = [];
+  //  Grab all book elements
+  const books = page.locator('.product_pod');
 
-for (let i = 0; i < 3; i++) {
-  const book = books.nth(i);
+  //  Prepare an array to store data
+  let bookDetails: Book[] = [];
+  let inStockCount = 0;
+ const totalcount = await books.count();
 
-  // 3. Grab details from current book card
-  const title = await book.locator('h3 a').getAttribute('title');
-  const price = await book.locator('.price_color').textContent();
-  const availability = await book.locator('.availability').textContent();
 
-  // 4. Push to array
-  bookDetails.push({
-    title,
-    price,
-    availability: availability?.trim()
-  });
-}
+  for (let i = 0; i < totalcount; i++) {
+    const book = books.nth(i);
 
-// 5. Output
-console.log(bookDetails);
-console.table(bookDetails);
-fs.writeFileSync("books.json", JSON.stringify(bookDetails, null, 2));
+    //  Grab details from current book card
+    const title = await book.locator('h3 a').getAttribute('title');
+    const price = await book.locator('.price_color').textContent();
+    const availability = await book.locator('.availability').textContent();
+    
+    if (availability?.includes('In stock')) {
+      inStockCount++;
+    }
+    
+    //  Push to array
+    bookDetails.push({
+      title,
+      price,
+      availability: availability?.trim()
+    });
+  }
+  console.log(totalcount);
+  console.log(`Items in stock: ${inStockCount}`);
+
+
+
+  //  Output
+  console.log(bookDetails);
+  console.table(bookDetails);
+
+  //fs.writeFileSync("books.json", JSON.stringify(bookDetails, null, 2));
 
 });
